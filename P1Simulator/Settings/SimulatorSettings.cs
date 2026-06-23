8a09abc00b307bfeca77e17e09c70213e79133a6
Geert Vancompernolle <geert.vancompernolle@gmail.com>
2026-06-23 19:20:51 +0200

    Change telegram speed to 850ms.  Looks like an ideal situation for the P1 live approach.

--- P1Simulator/Settings/SimulatorSettings.cs
+++ P1Simulator/Settings/SimulatorSettings.cs
@@ -4,7 +4,9 @@
     {
         public string Template { get; set; } = "3phase";
         public string Profile { get; set; } = "fixed";
-        public int SpeedMs { get; set; } = 750;
+        public int SpeedMs { get; set; } = 850; // Looks like the most optimal refresh frequency for the simulator:
+                                                // - no double entries in one second
+                                                // - no empty pages because of deduplication mechanism (if timestamp is same, send empty page)
         public int ConsoleWidth { get; set; } = 120;
         public int ConsoleHeight { get; set; } = 40;
 
